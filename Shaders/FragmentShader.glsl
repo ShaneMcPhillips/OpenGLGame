499de3f92db20d58d194906e56a4d670e711d02e
ShaneMcPhillips <smcphillips99@gmail.com>
2022-03-24 04:17:01 -0400

    Updated vertex and fragment shaders to incorporate simple texturing.

--- Shaders/FragmentShader.glsl
+++ Shaders/FragmentShader.glsl
@@ -1,7 +1,11 @@
 #version 330 core
+in vec2 texCoord;
+
+uniform sampler2D uTexture1;
+
 out vec4 color;
 
 void main() 
 {
-    color = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    color = texture(uTexture1, texCoord);
 }
